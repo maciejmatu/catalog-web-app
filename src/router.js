42d7257b48cf1cf815c42ff6273518dcd1a2b1fd
maciejmatu <maciek.matuszewski@gmail.comgit config --global user.name maciejmatu>
2017-07-17 00:52:31 +0200

    Create view for forgot-password scene

--- src/router.js
+++ src/router.js
@@ -17,6 +17,7 @@ const CustomRouter = () => (
       <Route exact path="/login" component={Login} />
       <Route exact path="/register" component={Register} />
       <Route exact path="/forgot-password" component={ForgotPassword} />
+      <Route exact path="/reset-password/:token" component={NotFound} />
       <Route component={NotFound} />
     </Switch>
   </Router>
--- src/scenes/ForgotPassword/index.jsx
+++ src/scenes/ForgotPassword/index.jsx
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Form, Icon, Input, Button } from 'antd';
 
-const ForgotPassword = () => (
-  <div className="ForgotPassword">
-    <div>Reset password</div>
-  </div>
-);
+import './styles.scss';
+import Logo from '../../components/Logo';
 
-export default ForgotPassword;
+class ForgotPassword extends Component {
+  static propTypes = {
+    form: PropTypes.object.isRequired
+  };
+
+  _handleSubmit = (e) => {
+    e.preventDefault();
+
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        return true;
+      } else {
+        console.log(err);
+      }
+    });
+  }
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+
+    return (
+      <div className="ForgotPassword__scene">
+        <Form onSubmit={this._handleSubmit} className="ForgotPassword__form">
+          <header className="ForgotPassword__form__header">
+            <div className="ForgotPassword__logo">
+              <Logo color="#000" />
+            </div>
+            <p>Pass the e-mail that you registered with, so
+              that we can send you a reset password link.
+            </p>
+          </header>
+          <Form.Item className="ForgotPassword__form__input">
+            {getFieldDecorator('email', {
+              rules: [
+                { type: 'email', message: 'The input is not valid E-mail!' },
+                { required: true, message: 'Please input your E-mail!' }
+              ]
+            })(
+              <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="Email" />
+            )}
+          </Form.Item>
+          <Button type="primary" htmlType="submit" className="RegisterForm__button">Submit</Button>
+        </Form>
+      </div>
+    );
+  }
+}
+
+export default Form.create()(ForgotPassword);
